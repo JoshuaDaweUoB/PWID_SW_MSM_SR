0acf16c4263631a31736c487ac34788f973a3c73
JoshuaDaweUoB <joshua.dawe@bristol.ac.uk>
2025-03-04 13:56:21 +0000

    first forest plot function

--- meta-analysis_code.R
+++ meta-analysis_code.R
@@ -1,5 +1,8 @@
 # load packages
-pacman::p_load("meta", "metafor", "DescTools", "car", "readxl", "tidyverse") 
+pacman::p_load("meta", "metafor", "DescTools", "car", "readxl", "tidyverse")
+
+# source functions
+source("C:/Users/vl22683/OneDrive - University of Bristol/Documents/Publications/Sex work and risk of HIV and HCV/code/code/meta-analysis_functions.r")
 
 # housekeeping
 settings.meta(CIbracket = "(") 
@@ -9,201 +12,27 @@ settings.meta(CIseparator = "-")
 hiv_sw_all <- read_excel("C:/Users/vl22683/OneDrive - University of Bristol/Documents/Publications/Sex work and risk of HIV and HCV/Data extraction/Full data extraction_24022025.xlsx", sheet = "HIV - Sex work - All") 
 hiv_sw_males <- read_excel("C:/Users/vl22683/OneDrive - University of Bristol/Documents/Publications/Sex work and risk of HIV and HCV/Data extraction/Full data extraction_24022025.xlsx", sheet = "HIV - Sex work - Male") 
 hiv_sw_females <- read_excel("C:/Users/vl22683/OneDrive - University of Bristol/Documents/Publications/Sex work and risk of HIV and HCV/Data extraction/Full data extraction_24022025.xlsx", sheet = "HIV - Sex work - Female") 
+hiv_msm <- read_excel("C:/Users/vl22683/OneDrive - University of Bristol/Documents/Publications/Sex work and risk of HIV and HCV/Data extraction/Full data extraction_24022025.xlsx", sheet = "HIV - MSM") 
 hcv_sw_all <- read_excel("C:/Users/vl22683/OneDrive - University of Bristol/Documents/Publications/Sex work and risk of HIV and HCV/Data extraction/Full data extraction_24022025.xlsx", sheet = "HCV - Sex work - All") 
 hcv_sw_males <- read_excel("C:/Users/vl22683/OneDrive - University of Bristol/Documents/Publications/Sex work and risk of HIV and HCV/Data extraction/Full data extraction_24022025.xlsx", sheet = "HCV - Sex work - Male") 
 hcv_sw_females <- read_excel("C:/Users/vl22683/OneDrive - University of Bristol/Documents/Publications/Sex work and risk of HIV and HCV/Data extraction/Full data extraction_24022025.xlsx", sheet = "HCV - Sex work - Female") 
+hcv_msm <- read_excel("C:/Users/vl22683/OneDrive - University of Bristol/Documents/Publications/Sex work and risk of HIV and HCV/Data extraction/Full data extraction_24022025.xlsx", sheet = "HCV - MSM") 
 
+# define dataframes
+dfs <- list(hiv_sw_all, hiv_sw_males, hiv_sw_females, hiv_msm, hcv_sw_all, hcv_sw_males, hcv_sw_females, hcv_msm)
+filenames <- c("sw_recent_hiv_all_unadj.png", "sw_recent_hiv_males_unadj.png", "sw_recent_hiv_females_unadj.png", "sw_recent_hiv_msm_unadj.png", "sw_recent_hcv_all_unadj.png", "sw_recent_hcv_males_unadj.png", "sw_recent_hcv_females_unadj.png", "sw_recent_hcv_msm_unadj.png")
 
+# data cleaning
 
-### sex work 
+# convert to numeric and log transform
+for (i in 1:length(dfs)) {
+  df <- dfs[[i]]
+  df <- convert_to_numeric(df)
+  df <- log_transform(df)
+  dfs[[i]] <- df
+}
 
-## HIV (males and females)
-
-
-
-########## sex work ############
-
-hiv_sw <- read_excel("C:/Users/joshua.dawe/OneDrive - University of Bristol/Documents/Publications/Sex work and risk of HIV and HCV/code/data/hiv data.xlsx", sheet = "sex_work") # load data
-hiv_sw <- read_excel("C:/Users/vl22683/OneDrive - University of Bristol/Documents/Publications/Sex work and risk of HIV and HCV/code/data/hiv data.xlsx", sheet = "sex_work") # load data
-
-hiv_sw <- transform(hiv_sw, cases = as.numeric(cases), 
-                         prs_yrs = as.numeric(prs_yrs),
-                         effect_LB_past = as.numeric(effect_LB_past),
-                         effect_UB_past = as.numeric(effect_UB_past)) # convert to numeric                        
-                      
-hiv_sw <- transform(hiv_sw, effect_past_ln = log(effect_past),
-                         effect_past_LB_ln = log(effect_LB_past),
-                         effect_past_UB_ln = log(effect_UB_past),
-                         effect_recent_ln = log(effect_recent),
-                         effect_recent_LB_ln = log(effect_LB_recent),
-                         effect_recent_UB_ln = log(effect_UB_recent)) # convert to log
-
-HIV_sw_recent <- hiv_sw %>% drop_na(effect_recent) # drop missing for aesthetics
-
-RR_HIV_sw_recent <- metagen(TE = effect_recent_ln,
-                           lower = effect_recent_LB_ln,
-                           upper = effect_recent_UB_ln,
-                           studlab = author_inci,
-                           data = HIV_sw_recent,
-                           sm = "RR",
-                           method.tau = "DL",
-                           comb.fixed = FALSE,
-                           comb.random = TRUE, 
-                           backtransf = TRUE,
-                           byvar = inci_publi_status,
-                           text.random = "Overall")
-summary(RR_HIV_sw_recent) # recent sex work
-
-png(filename = "C:/Users/vl22683/OneDrive - University of Bristol/Documents/Publications/Sex work and risk of HIV and HCV/code/plots/sw_recent_hiv.png", width = 30, height = 14, units = "cm",
-    res = 500)
-
-forest_sw <- forest.meta(RR_HIV_sw_recent, 
-                         sortvar = WHO_region,
-                         xlim=c(0.2, 4),             
-                         leftcols = c("author_inci", "country", "WHO_region", "inci_publi_status", "percent_sw"), 
-                         leftlabs = c("Study", "Country", "WHO region", "Publication status", "Sex work (%)"),
-                         digits = 1,
-                         digits.tau2=1,
-                         digits.I2=1,
-                         digits.pval.Q=3,
-                         col.inside = "black",
-                         subgroup.name = "",
-                         subgroup = F,
-                         print.byvar = FALSE,
-                         col.by="black") 
-dev.off()
-
-
-
-# forest plot
-
-########## msm ############
-
-hiv_msm <- read_excel("C:/Users/vl22683/OneDrive - University of Bristol/Documents/Sex work and risk of HIV and HCV/code/data/hiv data.xlsx", sheet = "msm") # load data
-
-# log of estimates
-hiv_msm <- transform(hiv_msm, effect_recent_ln = log(effect_recent),
-                    effect_recent_LB_ln = log(effect_LB_recent),
-                    effect_recent_UB_ln = log(effect_UB_recent))
-
-# recent msm
-HIV_msm_recent <- hiv_msm %>% drop_na(effect_recent)
-
-RR_HIV_msm_recent <- metagen(TE = effect_recent_ln,
-                            lower = effect_recent_LB_ln,
-                            upper = effect_recent_UB_ln,
-                            studlab = author_inci,
-                            data = HIV_msm_recent,
-                            sm = "RR",
-                            method.tau = "DL",
-                            comb.fixed = FALSE,
-                            comb.random = TRUE, backtransf = TRUE,
-                            title = "RR comparing HIV incidence between msm vs. no msm in PWID",
-                            text.random = "Overall")
-summary(RR_HIV_msm_recent)
-
-png(filename = "msm_recent.png", width = 28, height = 10, units = "cm",
-    res = 500)
-
-forest_msm <- forest.meta(RR_HIV_msm_recent, 
-                         sortvar = WHO_region,
-                         xlim=c(0.2, 4),             
-                         leftcols = c("author_inci", "country", "WHO_region", "percent_msm"), 
-                         leftlabs = c("Study", "Country", "WHO region", "MSM (%)"),
-                         digits = 1,
-                         digits.tau2=1,
-                         digits.I2=1,
-                         digits.pval.Q=3,
-                         col.inside = "black",
-                         col.by="black") 
-
-dev.off()
-
-### HCV sex work ###
-
-hcv_sw <- read_excel("C:/Users/vl22683/OneDrive - University of Bristol/Documents/Publications/Sex work and risk of HIV and HCV/code/data/hcv data.xlsx") # load data
-
-hcv_sw <- transform(hcv_sw, 
-                    effect_LB_recent = as.numeric(effect_LB_recent),
-                    effect_UB_recent = as.numeric(effect_UB_recent)) # convert to numeric                        
-
-hcv_sw <- transform(hcv_sw, effect_recent_ln = log(effect_recent),
-                    effect_LB_recent_ln = log(effect_LB_recent),
-                    effect_UB_recent_ln = log(effect_UB_recent)) # convert to log
-
-HCV_sw_recent <- hcv_sw %>% drop_na(effect_recent) # drop missing for aesthetics
-
-RR_HCV_sw_recent <- metagen(TE = effect_recent_ln,
-                            lower = effect_LB_recent_ln,
-                            upper = effect_UB_recent_ln,
-                            studlab = author_inci,
-                            data = HCV_sw_recent,
-                            sm = "RR",
-                            method.tau = "DL",
-                            comb.fixed = FALSE,
-                            comb.random = TRUE, backtransf = TRUE,
-                            byvar = inci_publi_status,
-                            text.random = "Overall")
-summary(RR_HCV_sw_recent) # recent sex work
-
-png(filename = "C:/Users/vl22683/OneDrive - University of Bristol/Documents/Publications/Sex work and risk of HIV and HCV/code/plots/sw_recent_hcv.png", width = 28, height = 15, units = "cm",
-    res = 500)
-
-forest_sw_hcv <- forest.meta(RR_HCV_sw_recent, 
-                         sortvar = WHO_region,
-                         xlim=c(0.2, 4),             
-                         leftcols = c("author_inci", "country", "WHO_region", "percent_sw"), 
-                         leftlabs = c("Study", "Country", "WHO region", "Sex work (%)"),
-                         digits = 1,
-                         digits.tau2=1,
-                         digits.I2=1,
-                         digits.pval.Q=3,
-                         col.inside = "black",
-                         subgroup.name = "",
-                         subgroup = F,
-                         print.byvar = FALSE,
-                         col.by="black") 
-
-dev.off()
-
-# hcv msm
-
-hcv_msm <- read_excel("C:/Users/joshua.dawe/OneDrive - University of Bristol/Documents/Sex work and risk of HIV and HCV/code/data/hcv data.xlsx", sheet = "msm") # load data
-
-# log of estimates
-hcv_msm <- transform(hcv_msm, effect_recent_ln = log(effect_recent),
-                     effect_recent_LB_ln = log(effect_LB_recent),
-                     effect_recent_UB_ln = log(effect_UB_recent))
-
-HCV_msm_ever <- hcv_msm %>% drop_na(effect_recent)
-
-RR_HCV_msm_ever <- metagen(TE = effect_recent_ln,
-                             lower = effect_recent_LB_ln,
-                             upper = effect_recent_UB_ln,
-                             studlab = author_inci,
-                             data = HCV_msm_ever,
-                             sm = "RR",
-                             method.tau = "DL",
-                             comb.fixed = FALSE,
-                             comb.random = TRUE, backtransf = TRUE,
-                             title = "RR comparing HCV incidence between msm vs. no msm in PWID",
-                             text.random = "Overall")
-summary(RR_HCV_msm_ever)
-
-png(filename = "msm_hcv.png", width = 28, height = 10, units = "cm",
-    res = 500)
-
-forest_msm <- forest.meta(RR_HCV_msm_ever, 
-                          sortvar = WHO_region,
-                          xlim=c(0.2, 4),             
-                          leftcols = c("author_inci", "country", "WHO_region", "percent_msm"), 
-                          leftlabs = c("Study", "Country", "WHO region", "MSM (%)"),
-                          digits = 1,
-                          digits.tau2=1,
-                          digits.I2=1,
-                          digits.pval.Q=3,
-                          col.inside = "black",
-                          col.by="black") 
-
-dev.off()
-
+# Apply the function to each dataframe
+for (i in 1:length(dfs)) {
+  generate_forest_plot(dfs[[i]], "recent", "effect_unadj_ln", "effect_unadj_lb_ln", "effect_unadj_ub_ln", "lead_author", "pub_status", paste0("C:/Users/vl22683/OneDrive - University of Bristol/Documents/Publications/Sex work and risk of HIV and HCV/code/plots/", filenames[i]))
+}
--- meta-analysis_functions.r
+++ meta-analysis_functions.r
@@ -10,9 +10,9 @@ convert_to_numeric <- function(df) {
                   effect_adj1 = as.numeric(effect_adj1), 
                   effect_adj_lb1 = as.numeric(effect_adj_lb1),
                   effect_adj_ub1 = as.numeric(effect_adj_ub1),
-                  effect_adj1 = as.numeric(effect_adj2), 
-                  effect_adj_lb1 = as.numeric(effect_adj_lb2),
-                  effect_adj_ub1 = as.numeric(effect_adj_ub2))
+                  effect_adj2 = as.numeric(effect_adj2), 
+                  effect_adj_lb2 = as.numeric(effect_adj_lb2),
+                  effect_adj_ub2 = as.numeric(effect_adj_ub2))
   return(df)
 }
 
@@ -22,11 +22,52 @@ log_transform <- function(df) {
                   effect_unadj_ln = log(effect_unadj),
                   effect_unadj_lb_ln = log(effect_unadj_lb),
                   effect_unadj_ub_ln = log(effect_unadj_ub),
-                  adj_est_ln = log(adj_est),
-                  adj_lower_ln = log(adj_lower),
-                  adj_upper_ln = log(adj_upper),
-                  effect_best_ln = log(effect_best),
-                  effect_best_lower_ln = log(effect_best_lower),
-                  effect_best_upper_ln = log(effect_best_upper))
+                  effect_adj1_ln = log(effect_adj1),
+                  effect_adj_lb1_ln = log(effect_adj_lb1),
+                  effect_adj_ub1_ln = log(effect_adj_ub1),
+                  effect_adj2_ln = log(effect_adj2),
+                  effect_adj_lb2_ln = log(effect_adj_lb2),
+                  effect_adj_ub2_ln = log(effect_adj_ub2))
   return(df)
+}
+
+# meta analysis
+
+# Define the function to generate forest plots
+generate_forest_plot <- function(df, exposure_time_frame, effect_col, lower_col, upper_col, studlab_col, byvar_col, filename) {
+  filtered_df <- df %>% 
+    filter(exposure_time_frame == "recent") %>% 
+    filter(!is.na(moa_unadj))
+  
+  forest_plot <- metagen(TE = effect_unadj_ln,
+                         lower = effect_unadj_lb_ln,
+                         upper = effect_unadj_ub_ln,
+                         studlab = lead_author,
+                         data = filtered_df,
+                         sm = "RR",
+                         method.tau = "DL",
+                         common = FALSE,
+                         random = TRUE, 
+                         backtransf = TRUE,
+                         subgroup = pub_status,
+                         text.random = "Overall")
+  summary(forest_plot)
+  
+  png(filename = filename, width = 30, height = 20, units = "cm", res = 500)
+  
+  forest_sw <- forest(forest_plot, 
+                      sortvar = lead_author,
+                      xlim = c(0.2, 4),             
+                      leftcols = c("country", "cohort"), 
+                      leftlabs = c("Country", "Cohort"),
+                      digits = 2,
+                      digits.tau2 = 1,
+                      digits.I2 = 1,
+                      digits.pval.Q = 3,
+                      col.inside = "black",
+                      subgroup.name = "",
+                      subgroup = TRUE,
+                      print.byvar = FALSE,
+                      col.by = "black") 
+  dev.off()
 }
