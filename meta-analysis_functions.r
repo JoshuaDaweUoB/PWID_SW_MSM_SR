08e35ee2ca3ea5b8e1022f2eab5c628ebedb6918
JoshuaDaweUoB <joshua.dawe@bristol.ac.uk>
2025-04-22 11:03:21 +1000

    subgroup function

--- meta-analysis_functions.r
+++ meta-analysis_functions.r
@@ -90,7 +90,7 @@ recent_unadj_forest_plot <- function(df, exposure_time_frame, effect_col, lower_
   forest_plot <- metagen(TE = effect_unadj_ln,
                          lower = effect_unadj_lb_ln,
                          upper = effect_unadj_ub_ln,
-                         studlab = lead_author,
+                         studlab = study,
                          data = filtered_df,
                          sm = "RR",
                          method.tau = "DL",
@@ -105,7 +105,7 @@ recent_unadj_forest_plot <- function(df, exposure_time_frame, effect_col, lower_
   png(filename = filename, width = 30, height = 20, units = "cm", res = 500)
   
   forest_sw <- forest(forest_plot, 
-                      sortvar = lead_author,
+                      sortvar = study,
                       xlim = c(0.2, 4),             
                       leftcols = c("country", "cohort"), 
                       leftlabs = c("Country", "Cohort"),
@@ -131,7 +131,7 @@ recent_adj1_forest_plot <- function(df, exposure_time_frame, effect_col, lower_c
   forest_plot <- metagen(TE = effect_adj1_ln,
                          lower = effect_adj_lb1_ln,
                          upper = effect_adj_ub1_ln,
-                         studlab = lead_author,
+                         studlab = study,
                          data = filtered_df,
                          sm = "RR",
                          method.tau = "DL",
@@ -146,7 +146,7 @@ recent_adj1_forest_plot <- function(df, exposure_time_frame, effect_col, lower_c
   png(filename = filename, width = 30, height = 20, units = "cm", res = 500)
   
   forest_sw <- forest(forest_plot, 
-                      sortvar = lead_author,
+                      sortvar = study,
                       xlim = c(0.2, 4),             
                       leftcols = c("country", "cohort"), 
                       leftlabs = c("Country", "Cohort"),
@@ -172,7 +172,7 @@ recent_adj2_forest_plot <- function(df, exposure_time_frame, effect_col, lower_c
   forest_plot <- metagen(TE = effect_adj2_ln,
                          lower = effect_adj_lb2_ln,
                          upper = effect_adj_ub2_ln,
-                         studlab = lead_author,
+                         studlab = study,
                          data = filtered_df,
                          sm = "RR",
                          method.tau = "DL",
@@ -187,7 +187,7 @@ recent_adj2_forest_plot <- function(df, exposure_time_frame, effect_col, lower_c
   png(filename = filename, width = 30, height = 20, units = "cm", res = 500)
   
   forest_sw <- forest(forest_plot, 
-                      sortvar = lead_author,
+                      sortvar = study,
                       xlim = c(0.2, 4),             
                       leftcols = c("country", "cohort"), 
                       leftlabs = c("Country", "Cohort"),
@@ -213,7 +213,7 @@ recent_best_forest_plot <- function(df, exposure_time_frame, effect_col, lower_c
   forest_plot <- metagen(TE = effect_best_ln,
                          lower = effect_best_lb_ln,
                          upper = effect_best_ub_ln,
-                         studlab = lead_author,
+                         studlab = study,
                          data = filtered_df,
                          sm = "RR",
                          method.tau = "DL",
@@ -228,7 +228,7 @@ recent_best_forest_plot <- function(df, exposure_time_frame, effect_col, lower_c
   png(filename = filename, width = 30, height = 20, units = "cm", res = 500)
   
   forest_sw <- forest(forest_plot, 
-                      sortvar = lead_author,
+                      sortvar = study,
                       xlim = c(0.2, 4),             
                       leftcols = c("country", "cohort"), 
                       leftlabs = c("Country", "Cohort"),
@@ -243,3 +243,76 @@ recent_best_forest_plot <- function(df, exposure_time_frame, effect_col, lower_c
                       col.subgroup = "black") 
   dev.off()
 }
+
+# subgroup analysis of recent best estimates
+
+# Function to conduct subgroup analyses
+subgroup_analysis <- function(df, exposure_time_frame, effect_col, lower_col, upper_col, studlab_col, subgroup_vars, output_dir) {
+  # Filter the dataframe for recent exposure time frame
+  filtered_df <- df %>%
+    filter(exposure_time_frame_bin == "recent") %>%
+    filter(!is.na(effect_best)) %>%
+    filter(effect_best != "NR")
+  
+  # Loop through each subgroup variable
+  for (subgroup_var in subgroup_vars) {
+    # Generate the forest plot for the current subgroup
+    forest_plot <- metagen(
+      TE = filtered_df[[effect_col]],
+      lower = filtered_df[[lower_col]],
+      upper = filtered_df[[upper_col]],
+      studlab = filtered_df[[studlab_col]],
+      data = filtered_df,
+      sm = "RR",
+      method.tau = "DL",
+      common = FALSE,
+      random = TRUE,
+      backtransf = TRUE,
+      subgroup = filtered_df[[subgroup_var]],
+      text.random = "Overall"
+    )
+    
+    # Print summary of the forest plot
+    summary(forest_plot)
+    
+    # Define the output filename
+    filename <- file.path(output_dir, paste0("forest_plot_", subgroup_var, ".png"))
+    print(paste("Saving plot to:", filename))  # Print the filename to confirm
+    
+    # Save the forest plot as a PNG
+    png(filename = filename, width = 30, height = 20, units = "cm", res = 500)
+    forest(
+      forest_plot,
+      sortvar = filtered_df[[studlab_col]],
+      xlim = c(0.2, 4),
+      leftcols = c("country", "cohort"),
+      leftlabs = c("Country", "Cohort"),
+      digits = 2,
+      digits.tau2 = 1,
+      digits.I2 = 1,
+      digits.pval.Q = 3,
+      col.inside = "black",
+      subgroup.name = subgroup_var,
+      subgroup = TRUE,
+      print.byvar = FALSE,
+      col.subgroup = "black"
+    )
+    dev.off()
+  }
+}
+
+# Example usage of the subgroup_analysis function
+subgroup_vars <- c("pub_status", "2016_bin", "incidence_method", "who_region", "lmic_4cat", "hiv_crim", "rob_3cat")
+output_dir <- "path/to/output/directory"  # Replace with your desired output directory
+
+# Call the function for subgroup analysis
+subgroup_analysis(
+  df = your_dataframe,  # Replace with your dataframe
+  exposure_time_frame = "recent",
+  effect_col = "effect_best_ln",
+  lower_col = "effect_best_lb_ln",
+  upper_col = "effect_best_ub_ln",
+  studlab_col = "study",
+  subgroup_vars = subgroup_vars,
+  output_dir = output_dir
+)
